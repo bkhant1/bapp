4f556f0027f7a44257f99d64b3b667a318efdca5
macbook <macbook@M1-MacBook-Air.local>
2025-08-15 10:39:14 +0100

    Add private service connection for Cloud SQL networking

--- infra/main.tf
+++ infra/main.tf
@@ -57,6 +57,22 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.id
 }
 
+# Reserve IP range for private services
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "${var.project_name}-private-ip"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.id
+}
+
+# Create private service connection
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
 # Cloud SQL PostgreSQL Instance
 resource "google_sql_database_instance" "postgres" {
   name             = "${var.project_name}-db"
@@ -86,7 +102,10 @@ resource "google_sql_database_instance" "postgres" {
 
   deletion_protection = false
 
-  depends_on = [google_project_service.apis]
+  depends_on = [
+    google_project_service.apis,
+    google_service_networking_connection.private_vpc_connection
+  ]
 }
 
 # Database
